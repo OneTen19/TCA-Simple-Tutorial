a55415dd2b26071016543ba12d00313a3cd59e7f
OneTen19 <khy000426@naver.com>
2025-12-20 21:48:17 +0900

    [Feat] TCA Tutorial - Your first feature

--- TCA-Simple-Tutorial/CounterView.swift
+++ TCA-Simple-Tutorial/CounterView.swift
@@ -6,20 +6,71 @@
 //
 
 import SwiftUI
+import ComposableArchitecture
+
+@Reducer
+struct CounterFeature {
+    @ObservableState
+    struct State: Equatable {
+        var count = 0
+    }
+    
+    enum Action {
+        case incrementButtonTapped
+        case decrementButtonTapped
+    }
+    
+    var body: some Reducer<State, Action> {
+        Reduce { state, action in
+            switch action {
+            case .incrementButtonTapped:
+                state.count += 1
+                return .none
+                
+            case .decrementButtonTapped:
+                state.count -= 1
+                return .none
+            }
+        }
+    }
+}
 
 struct CounterView: View {
+    let store: StoreOf<CounterFeature>
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("\(store.count)")
+                .font(.largeTitle)
+                .padding()
+                .background(Color.black.opacity(0.1))
+                .cornerRadius(10)
+            HStack {
+                Button("-") {
+                    store.send(.decrementButtonTapped)
+                }
+                .font(.largeTitle)
+                .padding()
+                .background(Color.black.opacity(0.1))
+                .cornerRadius(10)
+                
+                Button("+") {
+                    store.send(.incrementButtonTapped)
+                }
+                .font(.largeTitle)
+                .padding()
+                .background(Color.black.opacity(0.1))
+                .cornerRadius(10)
+            }
         }
-        .padding()
     }
 }
 
 #Preview {
-    CounterView()
+  CounterView(
+    store: Store(initialState: CounterFeature.State()) {
+      CounterFeature()
+            ._printChanges()
+    }
+  )
 }
-
--- TCA-Simple-Tutorial/TCA_Simple_TutorialApp.swift
+++ TCA-Simple-Tutorial/TCA_Simple_TutorialApp.swift
@@ -6,12 +6,18 @@
 //
 
 import SwiftUI
+import ComposableArchitecture
 
 @main
 struct TCA_Simple_TutorialApp: App {
+    static let store = Store(initialState: CounterFeature.State()) {
+        CounterFeature()
+            ._printChanges()
+    }
+    
     var body: some Scene {
         WindowGroup {
-            CounterView()
+            CounterView(store: TCA_Simple_TutorialApp.store)
         }
     }
 }

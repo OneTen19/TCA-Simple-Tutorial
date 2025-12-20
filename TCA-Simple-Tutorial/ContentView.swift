918ee79f6bb92db86ef517a05e931d97c1327e55
OneTen19 <khy000426@naver.com>
2025-12-20 16:36:47 +0900

    Initial Commit

---
+++ TCA-Simple-Tutorial/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TCA-Simple-Tutorial
+//
+//  Created by OneTen on 12/20/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TCA-Simple-Tutorial/TCA_Simple_TutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCA_Simple_TutorialApp.swift
+//  TCA-Simple-Tutorial
+//
+//  Created by OneTen on 12/20/25.
+//
+
+import SwiftUI
+
+@main
+struct TCA_Simple_TutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

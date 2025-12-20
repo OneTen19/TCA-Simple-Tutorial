4982535352382673386ed886444b121d9a4e7850
OneTen19 <khy000426@naver.com>
2025-12-21 00:09:59 +0900

    [Feat] TCA Zero-to-Hero #2 - Effect: 비동기 처리와 외부 통신

--- TCA-Simple-Tutorial/CounterView.swift
+++ TCA-Simple-Tutorial/CounterView.swift
@@ -13,11 +13,14 @@ struct CounterFeature {
     @ObservableState
     struct State: Equatable {
         var count = 0
+        var isLoading = false
     }
     
     enum Action {
-        case incrementButtonTapped
-        case decrementButtonTapped
+        case incrementButtonTapped  // 증가 버튼 클릭
+        case decrementButtonTapped  // 감소 버튼 클릭
+        case delayedIncrementButtonTapped // 지연 증가 버튼 클릭
+        case incrementResponse // 1초가 지난 후 실제로 카운트를 올리라는 내부 액션
     }
     
     var body: some Reducer<State, Action> {
@@ -30,6 +33,23 @@ struct CounterFeature {
             case .decrementButtonTapped:
                 state.count -= 1
                 return .none
+                
+            case .delayedIncrementButtonTapped:
+                state.isLoading = true  // 로딩 시작
+                
+                return .run { send in
+                    // 복잡한 비동기 로직(API 호출, 타이머 등)을 수행.
+                    try await Task.sleep(nanoseconds: 1_000_000_000) // 1초 대기
+                    
+                    // 작업이 끝나면 다시 Action을 날려서 State 변경.
+                    await send(.incrementResponse)
+                }
+                
+            case .incrementResponse:
+                state.isLoading = false // 로딩 끝
+                state.count += 1
+                return .none
+                
             }
         }
     }
@@ -40,11 +60,16 @@ struct CounterView: View {
     
     var body: some View {
         VStack {
-            Text("\(store.count)")
-                .font(.largeTitle)
-                .padding()
-                .background(Color.black.opacity(0.1))
-                .cornerRadius(10)
+            
+            if store.isLoading {
+                ProgressView()
+                    .padding()
+            } else {
+                Text("\(store.count)")
+                    .font(.largeTitle)
+                    .padding()
+            }
+            
             HStack {
                 Button("-") {
                     store.send(.decrementButtonTapped)
@@ -61,16 +86,24 @@ struct CounterView: View {
                 .padding()
                 .background(Color.black.opacity(0.1))
                 .cornerRadius(10)
+                
+                Button("1초 뒤 증가") {
+                    store.send(.delayedIncrementButtonTapped)
+                }
+                .font(.largeTitle)
+                .padding()
+                .background(Color.black.opacity(0.1))
+                .cornerRadius(10)
             }
         }
     }
 }
 
 #Preview {
-  CounterView(
-    store: Store(initialState: CounterFeature.State()) {
-      CounterFeature()
-            ._printChanges()
-    }
-  )
+    CounterView(
+        store: Store(initialState: CounterFeature.State()) {
+            CounterFeature()
+                ._printChanges()
+        }
+    )
 }
